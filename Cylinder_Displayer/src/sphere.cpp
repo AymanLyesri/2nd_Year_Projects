0ce0880db304f4e93bb3aad66f304907102da68e
Ayman Lyesri <lyesri99@gmail.com>
2022-05-30 09:14:36 +0100

    new cylinder displayer

--- Cylinder_Displayer/src/sphere.cpp
+++ Cylinder_Displayer/src/sphere.cpp
@@ -3,13 +3,24 @@
 int main()
 {
     int rad, hight;
-    std::cout << "Enter the radius and hight of the cylindre." << std::endl;
-    std::cin >> rad >> hight;
+    std::cout << "Enter the radius (10->60) and hight (125->600) of the cylindre." << std::endl;
+
+    do
+    {
+        std::cout << "\nradius : ";
+        std::cin >> rad;
+    } while (rad < 10 || rad > 60);
+    do
+    {
+        std::cout << "\nhight : ";
+        std::cin >> hight;
+    } while (hight < 125 || hight > 600);
 
     DisplayInit();
     Cylindre c1(rad, hight);
 
     c1.CylindreInit();
+    std::cout << "\n------------------------------COMPLETE!!------------------------------\n";
     getch();
 
     return 0;
